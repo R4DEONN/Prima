a11370327884d1c284fd2b4667bf5dd59c0381ed
Daniil Dolgorukov <daniil.dolgorukov@ispring.com>
2025-05-01 15:02:22 +0300

    Смержил мастер

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,23 @@
+import {GrammarParser, TableGenerator} from "./TableGenerator/TableGenerator";
 import {GrammarOptimizer} from "./TableOptimizer/TableOptimizer";
 import * as fs from "node:fs";
 
 function main(argc: number, argv: string[])
 {
-	const inputStr = fs.readFileSync(argv[3], "utf8");
-	GrammarOptimizer.optimize(inputStr, (str) => process.stdout.write(str));
+    if (process.argv[2] == 'table')
+    {
+        const parser = new GrammarParser(process.argv[3]);
+        const grammar = parser.parseGrammar();
+
+        const generator = new TableGenerator(grammar);
+        generator.generateTable();
+    }
+    else if (process.argv[2] == 'optimize')
+    {
+        const inputStr = fs.readFileSync(argv[3], "utf8");
+        GrammarOptimizer.optimize(inputStr, (str) => console.log(str));
+    }
+
 }
 
 main(process.argv.length, process.argv);
