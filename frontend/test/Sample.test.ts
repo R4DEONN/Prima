339dab0ab0469a25840b8829fddce4041c325117
kirill.borisov <kirill.borisov@ispring.com>
2025-05-04 18:23:39 +0300

    Test

---
+++ frontend/test/Sample.test.ts
@@ -0,0 +1,10 @@
+import {describe, expect, test} from "@jest/globals";
+
+describe("mew", () => {
+    test("test", () => {
+        expect(1).toBe(1);
+        expect({'s': 's'}).toStrictEqual({'s': 's'});
+
+        expect(() => {throw new Error()}).toThrow()
+    });
+});
