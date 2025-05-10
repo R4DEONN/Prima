f776d77d005315920d72659f42329afebf9c48c5
Daniil Dolgorukov <daniil.dolgorukov@ispring.com>
2025-05-10 18:14:50 +0300

    Удалил старый лексер

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,52 +1,24 @@
-import {GrammarOptimizer} from "./TableOptimizer/TableOptimizer";
-import {SyntacticRecognizer} from "./SyntacticRecognizer/SyntacticRecognizer";
-import * as fs from "node:fs";
+import {GrammarProcessor} from "./TableGenerator/Processor/GrammarProcessor";
 import {Scanner} from "./Scanner/Scanner";
+import {ConsoleReporter} from "./Error/ErrorReporter/ConsoleReporter";
+import fs from "node:fs";
 
 function main(argc: number, argv: string[])
 {
-	// const inputStr = fs.readFileSync(argv[3], "utf8");
-	// GrammarOptimizer.optimize(inputStr, (str) => process.stdout.write(str));
-	const primeNumb = new Scanner();
-	const table = {
-		"<Z>": {
-			"<Z>": "OK",
-			"<S>": "<S>0,1",
-			"~real~": "~real~1,1"
-		},
-		"<S>0,1": {
-			"~#~": "R<Z>1"
-		},
-		"~real~1,1": {
-			"<idlist>": "<idlist>1,2+<idlist>3,1",
-			"<id>": "<id>3,1",
-			"~A~": "~A~4,1"
-		},
-		"<idlist>1,2+<idlist>3,1": {
-			"~,~": "~,~2,2",
-			"~#~": "R<S>2"
-		},
-		"<id>3,1": {
-			"~,~": "R<idlist>1",
-			"~#~": "R<idlist>1"
-		},
-		"~A~4,1": {
-			"~,~": "R<id>1",
-			"~#~": "R<id>1"
-		},
-		"~,~2,2": {
-			"<id>": "<id>2,3",
-			"~A~": "~A~4,1"
-		},
-		"<id>2,3": {
-			"~,~": "R<idlist>3",
-			"~#~": "R<idlist>3"
-		}
-	};
-
-	const input = "~real~ ~A~ ~,~ ~A~ ~,~ ~A~ ~,~ ~A~ ~#~";
-	const recognizer = new SyntacticRecognizer(JSON.stringify(table));
-	console.log(recognizer.parse(input));
+    if (argv[2] == "table")
+    {
+        const processor = new GrammarProcessor("grammar.txt");
+        processor.processGrammar();
+        processor.exportTable("output.json");
+    }
+    else if (argv[2] == "recognize")
+    {
+        const errorReporter = new ConsoleReporter();
+        const source = fs.readFileSync("input.txt", 'utf8');
+        const scanner = new Scanner(source, errorReporter);
+        const tokens = scanner.scanTokens()
+        console.log(tokens);
+    }
 }
 
 main(process.argv.length, process.argv);
